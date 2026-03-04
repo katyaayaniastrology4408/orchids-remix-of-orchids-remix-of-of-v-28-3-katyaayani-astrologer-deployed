77923d87fb068f2b3f80eae4808e19591f747cc3
katyaayaniastrologer <katyaayaniastrologer01@gmail.com>
2026-03-04 13:37:31 +0530

    add

--- src/app/blog/[slug]/BlogPostClient.tsx
+++
@@ -1,4 +0,0 @@
-// THIS FILE IS NO LONGER USED.
-// ALL BLOG POST CLIENT LOGIC HAS BEEN MOVED TO @/components/blog/BlogPostClient.tsx
-// TO AVOID HYDRATION ERRORS AND DUPLICATION.
-export default function BlogPostClient() { return null; }
