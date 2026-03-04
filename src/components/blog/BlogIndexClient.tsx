ad0171ebf1c9824632dfcb61eb4f611338e65cb8
katyaayaniastrologer <katyaayaniastrologer01@gmail.com>
2026-03-04 13:41:10 +0530

    fixerror

--- src/components/blog/BlogIndexClient.tsx
+++ src/components/blog/BlogIndexClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { motion } from "framer-motion";
